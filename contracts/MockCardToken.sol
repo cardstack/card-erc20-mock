6eb9eba207d57df870c3c033fca2c1f7a69e271f
Alex Speller <alex@alexspeller.com>
2022-08-11 14:57:40 +0100

    Use super to call super method that is overriden in child contract

--- contracts/MockCardToken.sol
+++ contracts/MockCardToken.sol
@@ -29,9 +29,7 @@ contract MockCardToken is ERC20PresetMinterPauser {
     virtual
     returns (bool)
   {
-    address owner = _msgSender();
-    _approve(owner, spender, allowance(owner, spender) + addedValue);
-    return true;
+    return super.increaseAllowance(spender, addedValue);
   }
 
   function decreaseApproval(address spender, uint256 subtractedValue)
@@ -39,17 +37,7 @@ contract MockCardToken is ERC20PresetMinterPauser {
     virtual
     returns (bool)
   {
-    address owner = _msgSender();
-    uint256 currentAllowance = allowance(owner, spender);
-    require(
-      currentAllowance >= subtractedValue,
-      "ERC20: decreased allowance below zero"
-    );
-    unchecked {
-      _approve(owner, spender, currentAllowance - subtractedValue);
-    }
-
-    return true;
+    return super.decreaseAllowance(spender, subtractedValue);
   }
 
   // These are inherited from the OZ contract and are similar enough to the ones in the CARD token
